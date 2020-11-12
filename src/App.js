47ce994f2f43ded1398debfd0b2fcb64056589bd
jito <jitokj3@gmail.com>
2020-11-12 20:32:03 +0530

    fetched list of countries

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
+import React, { useEffect, useState } from "react";
+import {FormControl,MenuItem,Select} from "@material-ui/core";
 
 import './App.css';
 
+
 function App() {
+
+  const [countries,setCountries] = useState([]);
+
+  useEffect(()=>{
+       fetch("https://disease.sh/v3/covid-19/countries")
+      .then((response)=>response.json())
+      .then((data)=>{
+        const countries = data.map((country)=>({
+          name: country.country,
+          value: country.countryInfo.iso2
+        }))
+        setCountries(countries);
+      })
+  },[]);
+
   return (
     <div className="app">
-     <h1>Hello World</h1>
+    <div className="app__header">
+    <h1>COVID-19 TRACKER</h1>
+     <FormControl className="app__dropdown">
+       <Select variant="outlined" value="abc">
+       {
+         countries.map((country)=>(
+          <MenuItem value={country.value}>{country.name}</MenuItem>
+     
+         ))
+       }
+     
+        
+       </Select>
+     </FormControl>
+
+    </div>
+    
     </div>
   );
 }
