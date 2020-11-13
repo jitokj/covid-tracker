5d5ac2575d55bad071b4857f21d4b8fc2c7aebe3
jito <jitokj3@gmail.com>
2020-11-13 13:19:40 +0530

    added country table

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import "./Table.css";
 
 const Table = ({countries}) => {
+    // console.log(countries);
     return (
         <div className="table">
-            {countries.map(({country,cases})=>(
-                <tr>
+        <table>
+         <tbody>
+            {countries.map(({country,cases,countryInfo})=>(
+              
+                <tr key={countryInfo._id === null ? Math.random(3) : countryInfo._id}>
                     <td>{country}</td>
                     <td><strong>{cases}</strong></td>
                 </tr>
+                
             ))}
+            </tbody>
+            </table>
         </div>
     );
 };
